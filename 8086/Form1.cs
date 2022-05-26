16ca350c6ba826832c29ea1076e7f429b7102e9c
tigil <tigil@DESKTOP-MDBHTQL>
2022-05-26 21:30:25 +0200

    Add project files.

---
+++ 8086/Form1.cs
@@ -0,0 +1,20 @@
+namespace _8086
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
